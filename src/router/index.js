fc15e798ec45fea48f65762e40944c419e1b730b
JOKING <754037025@qq.com>
2018-11-24 16:48:18 +0800

    基础搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/views/Home'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const constRouteMap = [{
+  path: '/hello',
+  name: 'HelloWorld',
+  component: HelloWorld
+}, {
+  path: '/',
+  name: 'Home',
+  component: Home,
+  meta: {
+    title: 'Home Page'
+  }
+}]
+const router = new Router({
+  routes: constRouteMap
 })
+export default router
